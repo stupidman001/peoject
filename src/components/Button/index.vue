c228f6bac8af29b2c67e1b2f2c36fb713969d237
qiangyujun <qiangyujun@jd.com>
2021-10-28 21:01:57 +0800

    按钮的样式

--- src/components/Button/index.vue
+++ src/components/Button/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <button class="YUJUN-button">按钮组件</button>
+    <button class="YUJUN-button">
+      按钮组件
+    </button>
 </template>
 
 <script>
@@ -30,11 +32,48 @@ export default {
       type: Boolean,
     },
   },
-  created(){
-      console.log(this.type,this.plain,this.round,this.circle,this.disabled,this.icon)
-  }
+  created() {
+    console.log(
+      this.type,
+      this.plain,
+      this.round,
+      this.circle,
+      this.disabled,
+      this.icon
+    );
+  },
 };
 </script>
 
 <style lang="scss">
+@include b(YUJUN-button) {
+  display: inline-block;
+  line-height: 1;
+  white-space: nowrap;
+  cursor: pointer;
+  background: #ffffff;
+  border: 1px solid #dcdfe6;
+  color: #606266;
+  -webkit-appearance: none;
+  text-align: center;
+  box-sizing: border-box;
+  outline: none;
+  margin: 0;
+  transition: 0.1s;
+  font-weight: 500;
+  //禁止元素的文字被选中
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  padding: 12px 20px;
+  font-size: 14px;
+  border-radius: 4px;
+  &:hover,
+  &:hover {
+    color: #409eff;
+    border-color: #c6e2ff;
+    background-color: #ecf5ff;
+  }
+}
 </style>
